bfc410f234e85f36ab016e6a8931aee32b8e8d9d
ralonsodeniz <ralonsodeniz@gmail.com>
2023-04-04 16:45:30 +0200

    refactor(pages): home
    · Refactor home page to use SignIn, CreatePost and Feed components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 
-import { api } from "~/utils/api";
 import CreatePost from "~/components/CreatePost";
-import PostView from "~/components/PostView";
+import SignIn from "~/components/SignIn";
+import Feed from "~/components/Feed";
 
 const Home: NextPage = () => {
-  const user = useUser();
-  const { data, isLoading } = api.post.getAll.useQuery();
+  const { isSignedIn, isLoaded, user } = useUser();
 
   return (
     <>
@@ -17,28 +16,10 @@ const Home: NextPage = () => {
         <meta name="description" content="t3 test app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex h-screen justify-center">
-        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
-          <div className="flex flex-col justify-center border-b border-slate-400 p-4 ">
-            {!user.isSignedIn ? (
-              <SignInButton />
-            ) : (
-              <>
-                <SignOutButton />
-                <CreatePost />
-              </>
-            )}
-          </div>
-          <div className="flex flex-col">
-            {!!data
-              ? data.map(({ post, author }) => (
-                  <PostView post={post} author={author} key={post.id} />
-                ))
-              : isLoading
-              ? "Loading tRPC query..."
-              : "Error loading data"}
-          </div>
-        </div>
+      <main className="flex flex-col h-screen mx-auto w-full border-x border-slate-400 md:max-w-2xl">
+        <SignIn isLoaded={isLoaded} isSignedIn={isSignedIn} />
+        <CreatePost isLoaded={isLoaded} isSignedIn={isSignedIn} user={user} />
+        <Feed />
       </main>
     </>
   );
