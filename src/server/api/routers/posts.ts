28a7c79dc6b9eb08dcedba44d68e7bea7c118ff0
ralonsodeniz <ralonsodeniz@gmail.com>
2023-04-04 19:25:32 +0200

    feat(api): posts
    · Add create post method

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,13 +1,20 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import { filterUsersForClient } from "~/server/api/utils/users";
 import { TRPCError } from "@trpc/server";
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({ take: 100 });
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    });
     const users = (
       await clerkClient.users.getUserList({
         userId: posts.map((post) => post.authorId),
@@ -33,4 +40,20 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.currentUser;
+
+      return await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+    }),
 });
