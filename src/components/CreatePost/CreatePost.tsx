edb039ea7d78335090537dc18721a8c94e7979a9
ralonsodeniz <ralonsodeniz@gmail.com>
2023-04-04 16:44:53 +0200

    refactor(components): create-post
    · Add CreatePost Skeleton
    · Refactor CreatePost to implement Skeleton

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -1,19 +1,36 @@
-import { useUser } from "@clerk/nextjs";
 import { enLocale } from "public/locales";
 import Avatar from "~/components/Avatar";
+import { type UserResource } from "@clerk/types";
+import Skeleton from "~/components/CreatePost/Skeleton";
 
-const CreatePost = () => {
-  const { user } = useUser();
-
-  return user ? (
-    <div className="flex gap-4">
-      <Avatar profileImageUrl={user.profileImageUrl} />
-      <input
-        placeholder={enLocale["create-post"].input.placeholder}
-        className="grow bg-transparent outline-none"
-      />
-    </div>
-  ) : null;
-};
+const CreatePost = ({
+  user,
+  isLoaded,
+  isSignedIn,
+}: {
+  user?: UserResource | null;
+  isLoaded: boolean;
+  isSignedIn?: boolean;
+}) => (
+  <div className="flex flex-col border-b border-slate-400 p-4">
+    {isLoaded ? (
+      isSignedIn && user ? (
+        <div className="flex gap-4">
+          <Avatar profileImageUrl={user.profileImageUrl} />
+          <input
+            placeholder={enLocale["create-post"].input.placeholder}
+            className="grow bg-transparent outline-none"
+          />
+        </div>
+      ) : (
+        <div className="flex min-h-[56px] items-center justify-center">
+          {enLocale["create-post"]["sign-in"]}
+        </div>
+      )
+    ) : (
+      <Skeleton />
+    )}
+  </div>
+);
 
 export default CreatePost;
